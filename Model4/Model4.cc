9def4237cce63da907a41d5e7e8e13329e2dd959
Baptiste J Lemaire <blemaire@g.harvard.edu>
2019-05-15 17:42:28 -0400

    Corrected Barnes Hut

--- Model4/Model4.cc
+++ Model4/Model4.cc
@@ -7,19 +7,19 @@
 #include <omp.h>
 
 int main(){
-  double T_final = 5.0;
+  double T_final = 100.0;
   double hi_step = 1.0;
   double tolerance = 0.000001;
   double J = 0.1;
   double K = 1;
   double N_intrvls = 2;
-/*
-  rk4 myRk4_1(5000, hi_step, tolerance, J, K, N_intrvls,0.1,0);
+  rk4 myRk4_1(1250, hi_step, tolerance, J, K, N_intrvls,0.5,0);
   double t1 = omp_get_wtime();
   myRk4_1.compute_solution(T_final);
   double t2 = omp_get_wtime();
   myRk4_1.terminate();
-*/
+
+/*
   for (double N_pwr=4.10; N_pwr<4.15; N_pwr += 0.05 ){
     int NN = (int) pow(10, N_pwr);
     rk4 myRk4_norm(NN, hi_step, tolerance, J, K, N_intrvls,0.1,0);
@@ -34,6 +34,7 @@ int main(){
     myRk4_bh.terminate();
     printf("%d %f %f\n",NN, t2-t1, t4-t3);
   }
+*/
 /*  
   for (double pwr=-2; pwr<-0.2; pwr+=0.02){
     double theta = pow(10,pwr);
@@ -65,6 +66,6 @@ int main(){
     printf("%f %f %f %f %f %f\n",theta,t2-t1,t4-t3,t6-t5,t8-t7,t10-t9);
   }
 */
-  //printf("Total Computation Time: %f\n", t2-t1);
+  printf("Total Computation Time: %f\n", t2-t1);
   return 0;
 }
